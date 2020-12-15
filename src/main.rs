38d3984d2b301191c214d178a184681c37d2c6f8
Roland Metivier <metivier.roland@chlorophyt.us>
2020-12-15 09:20:18 -0600

    initial items

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use serenity::async_trait;
+use serenity::client::{Client, Context, EventHandler};
+use serenity::model::channel::Message;
+use serenity::framework::standard::{
+    StandardFramework,
+    CommandResult,
+    macros::{
+        command,
+        group
+    }
+};
+
+use std::env;
+
+fn main() {
+    println!("Hello, world!");
+}
